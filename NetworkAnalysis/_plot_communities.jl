a931ee9549385dad18a9e727857e71d406126694
Yi Wang <wangyi9122@yahoo.com>
2026-02-11 20:02:04 +0100

    improved figure format

--- NetworkAnalysis/_plot_communities.jl
+++ NetworkAnalysis/_plot_communities.jl
@@ -3,23 +3,23 @@ npanel = length(Is)
 depths=otudata.Middle_depth |> sort |> unique;
 
 fig = Figure(size=(220*(npanel+0.8)+100,1000));
-axs = [Axis(fig[2,i], limits=(0,0.33,-depths[end]-10,-depths[1]+200), yticks=-depths, xticklabelsize=13, yticklabelsize=13,
-            xlabel="DNA damage", ylabel="depth (cm)",
+axs = [Axis(fig[2,i], limits=(0,0.33, -depths[end]-10, -depths[1]+200), yticks=(-depths, string.(round.(Int, depths))), xticklabelsize=13, yticklabelsize=13,
+            xlabel="DNA damage", ylabel="Depth (cm)",
             xlabelpadding=0, ylabelpadding=0) for i in 1:npanel]; # on axs dmg-depth is plotted, plus α and phylogenetic diversity
 hidedecorations!.(axs, label=false, ticklabels = false, grid=false);
 hideydecorations!.(axs[2:end], grid = false);
 #hidexdecorations!.(axs[2:end], label=false, ticklabels=false, grid=false);
 linkyaxes!(axs...);
 
-bxs = [Axis(fig[1,i], limits=(0,4,nothing,nothing), xlabel="phylogenetic distance", ylabel="no. connections", 
+bxs = [Axis(fig[1,i], limits=(0,4,nothing,nothing), xlabel="Phylogenetic distance", ylabel="No. connections", 
             xticklabelsize=13, yticklabelsize=13, xlabelpadding=0, ylabelpadding=0, 
-            title=titles[i]*"\n"*string(nsps[i])*" taxa") for i in 1:length(Is)]; # on bxs hist of phylo distance is plotted
+            title=titles[i]*"\n"*string(nsps[i])*" taxa", titlefont = :regular, titlesize = 15) for i in 1:length(Is)]; # on bxs hist of phylo distance is plotted
 hidedecorations!.(bxs, label=false, ticklabels = false);
 rowsize!(fig.layout, 2, Relative(4/5));
 colgap!(fig.layout, 10); rowgap!(fig.layout, 10);
 
 cxs = [Axis(fig[2,i], limits=(-100,230,-depths[end]-10,-depths[1]+200), yticks=-depths,
-            xlabel="species richness", ylabel="", xaxisposition=:top, xlabelsize=12,
+            xlabel="Species richness", ylabel="", xaxisposition=:top, xlabelsize=12,
             xlabelpadding=0, ylabelpadding=0) for i in 1:npanel]; # on cxs species richness are plotted
 hideydecorations!.(cxs); hidexdecorations!.(cxs, ticklabels=true);
 
